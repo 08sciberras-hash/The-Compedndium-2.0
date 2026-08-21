44ae1ec998b4bb7b729d5e2db2142ff7f4645c11
08sciberras-hash <08sciberras@gmail.com>
2026-08-21 12:20:16 +1000

    Bump cache after cloud sync bug fix

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE="bird-compendium-v4";const ASSETS=["./","./index.html","./styles.css","./app.js","./cloud-sync.js","./manifest.webmanifest","./icon-192.png","./icon-512.png","./apple-touch-icon.png"];
+const CACHE="bird-compendium-v4-1";const ASSETS=["./","./index.html","./styles.css","./app.js","./cloud-sync.js","./manifest.webmanifest","./icon-192.png","./icon-512.png","./apple-touch-icon.png"];
 self.addEventListener("install",e=>{self.skipWaiting();e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)))});
 self.addEventListener("activate",e=>e.waitUntil(Promise.all([self.clients.claim(),caches.keys().then(ks=>Promise.all(ks.filter(k=>k!==CACHE).map(k=>caches.delete(k))))])));
 self.addEventListener("fetch",e=>{if(e.request.method!=="GET")return;const u=new URL(e.request.url);if(u.origin===location.origin)e.respondWith(fetch(e.request).then(r=>{const c=r.clone();caches.open(CACHE).then(k=>k.put(e.request,c));return r}).catch(()=>caches.match(e.request)))});
